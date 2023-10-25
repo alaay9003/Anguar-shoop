2bd0e1d0b1072e8ea316ee890d5785e9ba661265
alaay9003 <alaay9003@gmail.com>
2023-10-25 17:05:01 +0300

    v1

--- src/app/products/components/all-products/all-products.component.ts
+++ src/app/products/components/all-products/all-products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../../services/products.service';
+import { Iproduct } from '../../models/IProduct';
 
 @Component({
   selector: 'app-all-products',
@@ -7,7 +8,7 @@ import { ProductsService } from '../../services/products.service';
   styleUrls: ['./all-products.component.scss'],
 })
 export class AllProductsComponent implements OnInit {
-  public products: any[] = [];
+  public products: Iproduct[] = [];
   Categories: any[] = [];
   cartProducts: any[] = [];
   constructor(private service: ProductsService) {}
--- src/app/products/components/product/product.component.ts
+++ src/app/products/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Iproduct } from './../../models/IProduct';
 
 @Component({
   selector: 'app-product',
